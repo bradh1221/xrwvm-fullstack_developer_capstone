55c7e545f60286204bf36437d0605cd3ab7866de
bradh1221 <brad_h_122182@yahoo.com>
2024-09-12 18:55:36 -0400

    added changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -33,7 +33,7 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake":
-            car_model.car_make.name})
+        car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -128,6 +128,6 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401, "message":
-            "Error in posting review"})
+                "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
