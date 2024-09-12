0785b853cc74ac96937585fd8b93966db61e2eed
bradh1221 <brad_h_122182@yahoo.com>
2024-09-12 17:53:27 -0400

    Files Updated

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
-
 # CarModelInline class
 
 # CarModelAdmin class
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,11 +4,6 @@ from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -20,7 +15,7 @@ class CarMake(models.Model):
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -30,7 +25,7 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
+            validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
